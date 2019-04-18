ea94bbe7ef41b6d74c146bc258c133c1aa68cae6
Mark Harvey <maharvey2@crimson.ua.edu>
2019-04-18 14:23:05 +0000

    Automatically backed up by Learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -4,3 +4,54 @@ def position_taken?(board, index)
 end
 
 # Define your WIN_COMBINATIONS constant
+WIN_COMBINATIONS = [
+  [0,1,2], #top
+  [3,4,5], #middle
+  [6,7,8], #bottom
+  [2,4,6], #R2L diagonal
+  [0,4,8], #L2R diagonal
+  [0,3,6], #down first column
+  [1,4,7], #down second column
+  [2,5,8] #down third column
+  ]
+# so how do you win?
+
+for each win_combination in WIN_COMBINATIONS do
+  # win_combination is a 3 element array of indexes that compose a win, [0,1,2]
+  # grab each index from the win_combination that composes a win.
+  win_index_1 = win_combination[0]
+  win_index_2 = win_combination[1]
+  win_index_3 = win_combination[2]
+  win_index_4 = win_combination[3]
+  win_index_5 = win_combination[4]
+  win_index_6 = win_combination[5]
+  win_index_7 = win_combination[6]
+  win_index_8 = win_combination[7]
+
+  position_1 = board[win_index_1] # load the value of the board at win_index_1
+  position_2 = board[win_index_2] # load the value of the board at win_index_2
+  position_3 = board[win_index_3] # load the value of the board at win_index_3
+  position_4 = board[win_index_4]
+  position_5 = board[win_index_5]
+  position_6 = board[win_index_6]
+  position_7 = board[win_index_7]
+
+
+  if position_1 == "X" && position_2 == "X" && position_3 == "X"
+    return win_combination # return the win_combination indexes that won.
+  else
+    false
+  end
+end
+
+
+#full?
+
+
+#draw
+
+
+#over
+
+
+#winner
