be69c1c63b123e07628c1a3efe4c0d69e2c67e35
Mark Harvey <maharvey2@crimson.ua.edu>
2019-04-18 14:38:14 +0000

    Automatically backed up by Learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -5,15 +5,15 @@ end
 
 # Define your WIN_COMBINATIONS constant
 WIN_COMBINATIONS = [
-  [0,1,2], #top
-  [3,4,5], #middle
-  [6,7,8], #bottom
-  [2,4,6], #R2L diagonal
-  [0,4,8], #L2R diagonal
-  [0,3,6], #down first column
-  [1,4,7], #down second column
-  [2,5,8] #down third column
-  ]
+  [0,1,2],
+  [3,4,5],
+  [6,7,8],
+  [0,3,6],
+  [1,4,7],
+  [2,5,8],
+  [0,4,8],
+  [6,4,2]
+]
 # so how do you win?
 def won?(board)
   if position_taken(board,index)
