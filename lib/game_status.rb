2afaf158f7f8119a3029e2fd794ca88ca4d2de5f
Mark Harvey <maharvey2@crimson.ua.edu>
2019-04-18 14:40:17 +0000

    Automatically backed up by Learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -14,16 +14,28 @@ WIN_COMBINATIONS = [
   [0,4,8],
   [6,4,2]
 ]
-# so how do you win?
+def won?(board)
+  WIN_COMBINATIONS.detect do |combo|
+    board[combo[0]] == board[combo[1]] &&
+    board[combo[1]] == board[combo[2]] &&
+    position_taken?(board, combo[0])
+  end
+end
 
+def full?(board)
+  board.all?{|token| token == "X" || token == "O"}
+end
 
-#full?
+def draw?(board)
+  full?(board) && !won?(board)
+end
 
+def over?(board)
+  won?(board) || full?(board)
+end
 
-#draw
-
-
-#over
-
-
-#winner
+def winner(board)
+  if winning_combo = won?(board)
+    board[winning_combo.first]
+  end
+end
